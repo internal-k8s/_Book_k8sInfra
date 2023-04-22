dfc378545e5ab7fa06f4247f30c40dc588c976b5
Hoon Jo <pagaia@hotmail.com>
2023-04-22 13:20:33 +0900

    upt 2.2.3

---
+++ ch2/2.2.3/ping_2_nds.sh
@@ -0,0 +1,4 @@
+# ping 1 time per node
+ping 192.168.1.101 -c 1
+ping 192.168.1.102 -c 1
+ping 192.168.1.103 -c 1
---
+++ ch2/2.2.3/update_n_install_pkg.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# update package list 
+apt-get update 
+# install lsb-release
+apt-get install -y lsb-release 

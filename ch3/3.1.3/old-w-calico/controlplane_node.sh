4cdf72c5f2e987a916529fec734e68b96aa983d2
Hoon Jo <pagaia@hotmail.com>
2023-06-04 08:49:05 +0900

    rpl calico-ver

---
+++ ch3/3.1.3/old-w-calico/controlplane_node.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Fixed Internal-IP (temp) when eth1 could fix by autodetect calico 
+cat <<EOF > /etc/default/kubelet
+KUBELET_EXTRA_ARGS=--node-ip=192.168.1.10
+EOF
+
+# init kubernetes (w/ containerd)
+kubeadm init --token 123456.1234567890123456 --token-ttl 0 \
+             --pod-network-cidr=172.16.0.0/16 --apiserver-advertise-address=192.168.1.10 \
+             --cri-socket=unix:///run/containerd/containerd.sock
+
+# config for control-plane node only 
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+# raw_address for gitcontent
+raw_git="raw.githubusercontent.com/sysnet4admin/IaC/master/manifests" 
+
+# config for kubernetes's network 
+kubectl apply -f https://$raw_git/172.16_eht1_net_calico_v3.25.0.yaml
+
+# kubectl completion on bash-completion dir
+kubectl completion bash >/etc/bash_completion.d/kubectl
+
+# alias kubectl to k 
+echo 'alias k=kubectl' >> ~/.bashrc
+echo "alias ka='kubectl apply -f'" >> ~/.bashrc
+echo 'complete -F __start_kubectl k' >> ~/.bashrc
+
+# git clone book source 
+git clone https://github.com/internal-k8s/_Book_k8sInfra.git 
+mv /home/vagrant/_Book_k8sInfra $HOME
+find $HOME/_Book_k8sInfra -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+
+# make rerepo-k8s-learning.kit and put permission
+cat <<EOF > /usr/local/bin/rerepo-book-k8sinfra
+#!/usr/bin/env bash
+rm -rf $HOME/_Book_k8sInfra
+git clone https://github.com/internal-k8s/_Book_k8sInfra.git $HOME/_Book_k8sInfra
+find $HOME/_Book_k8sInfra -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+EOF
+chmod 700 /usr/local/bin/rerepo-book-k8sinfra
+

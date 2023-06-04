4527f20bf42041059069b1987511d8e85a10dd8a
Hoon Jo <pagaia@hotmail.com>
2023-06-04 12:33:14 +0900

    chg from lower to uppper

--- app/A.console-k8s/grap_kubeconfig.sh
+++ app/A.console-k8s/grap_kubeconfig.sh
@@ -6,15 +6,15 @@ mkdir ~/.kube
 # copy kubeconfig by sshpass
 sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no root@192.168.1.10:/etc/kubernetes/admin.conf ~/.kube/config 
 
-# git clone Book-k8sinfra code 
-git clone https://github.com/internal-k8s/_Book_k8sinfra.git $HOME/_Book_k8sinfra
-find $HOME/_Book_k8sinfra -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+# git clone Book-k8sInfra code 
+git clone https://github.com/internal-k8s/_Book_k8sInfra.git $HOME/_Book_k8sInfra
+find $HOME/_Book_k8sInfra -regex ".*\.\(sh\)" -exec chmod 700 {} \;
 
-# make rerepo-Book-k8sinfra and input proper permission
-cat <<EOF > /usr/local/bin/rerepo-Book_k8sinfra
+# make rerepo-Book-k8sInfra and input proper permission
+cat <<EOF > /usr/local/bin/rerepo-Book_k8sInfra
 #!/usr/bin/env bash
-rm -rf $HOME/_Book_k8sinfra
-git clone https://github.com/internal-k8s/_Book_k8sinfra.git $HOME/_Book_k8sinfra
-find $HOME/_Book_k8sinfra -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+rm -rf $HOME/_Book_k8sInfra
+git clone https://github.com/internal-k8s/_Book_k8sInfra.git $HOME/_Book_k8sInfra
+find $HOME/_Book_k8sInfra -regex ".*\.\(sh\)" -exec chmod 700 {} \;
 EOF
-chmod 700 /usr/local/bin/rerepo-Book_k8sinfra
+chmod 700 /usr/local/bin/rerepo-Book_k8sInfra

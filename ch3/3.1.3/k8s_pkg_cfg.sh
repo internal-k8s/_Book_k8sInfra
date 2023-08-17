3422491bca1660889fda2c526a9feb27f39bc1cf
Hoon Jo <pagaia@hotmail.com>
2023-08-17 11:09:04 +0900

    fix crictl endpoints issue

--- ch3/3.1.3/k8s_pkg_cfg.sh
+++ ch3/3.1.3/k8s_pkg_cfg.sh
@@ -22,6 +22,12 @@ apt-get install -y kubelet=$1 kubectl=$1 kubeadm=$1 containerd.io=$2
 containerd config default > /etc/containerd/config.toml
 sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 
+# Avoid WARN&ERRO(default endpoints) when crictl run  
+cat <<EOF > /etc/crictl.yaml
+runtime-endpoint: unix:///run/containerd/containerd.sock
+image-endpoint: unix:///run/containerd/containerd.sock
+EOF
+
 # ready to install for k8s 
 systemctl restart containerd ; systemctl enable containerd
 systemctl enable --now kubelet

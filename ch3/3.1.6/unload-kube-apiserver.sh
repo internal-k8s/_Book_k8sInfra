f37bc8078dc6d0e356f9919883decd2e44c9d750
Hoon Jo <pagaia@hotmail.com>
2023-08-17 14:28:56 +0900

    chg message

--- ch3/3.1.6/unload-kube-apiserver.sh
+++ ch3/3.1.6/unload-kube-apiserver.sh
@@ -3,6 +3,5 @@
 # unload k8s API-Server 
 cp /etc/kubernetes/manifests/kube-apiserver.yaml /tmp/kube-apiserver-backup.yaml 
 mv /etc/kubernetes/manifests/kube-apiserver.yaml /tmp/kube-apiserver.yaml 
-echo "Unloading kube-apiserver"
-echo "Wait for a mintue!"
+echo "Unloaded kube-apiserver"
 

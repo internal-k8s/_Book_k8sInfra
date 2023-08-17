50dd053470a0bb1c292c6cabaa6f5f2cee26cf79
Hoon Jo <pagaia@hotmail.com>
2023-08-17 14:40:59 +0900

    chg logic

--- ch3/3.1.6/load-kube-apiserver.sh
+++ ch3/3.1.6/load-kube-apiserver.sh
@@ -3,5 +3,5 @@
 # load k8s API-Server 
 mv /tmp/kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml 
 echo "Loading kube-apiserver"
-echo -n "Wait for 3 seconds." ; for i in {1..3} ; do echo -n ".";sleep 1 ; done
+echo "Wait for 1 minute!!!"
 

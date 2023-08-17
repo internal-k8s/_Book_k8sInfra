32a3818054981e79f0233ca747bcedfa3541bce8
Hoon Jo <pagaia@hotmail.com>
2023-08-17 10:12:27 +0900

    upt app.A

--- app/A.console-k8s/k8s_con_pack.sh
+++ app/A.console-k8s/k8s_con_pack.sh
@@ -6,14 +6,15 @@ export DEBIAN_FRONTEND=noninteractive
 # install util packages 
 apt-get install sshpass
 
-# add kubernetes repo
-apt-get update && apt-get install apt-transport-https ca-certificates curl
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
-           sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+# add kubernetes repo ONLY for 22.04
+mkdir -p /etc/apt/keyrings
+curl -fsSL \
+  https://packages.cloud.google.com/apt/doc/apt-key.gpg \
+  | gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
 echo \
-  "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] \
-  https://apt.kubernetes.io/ kubernetes-xenial main" | \
-  sudo tee /etc/apt/sources.list.d/kubernetes.list
+  "deb [signed-by=/etc/apt/keyrings/kubernetes.gpg] \
+  https://apt.kubernetes.io/ kubernetes-xenial main" \
+  | tee /etc/apt/sources.list.d/kubernetes.list
 
 # update repo info 
 apt-get update 
@@ -21,7 +22,7 @@ apt-get update
 # install kubectl
 apt-get install kubectl=$1 
 
-# kubectl completion on bash-completion dir due to completion already installed 
+# kubectl completion on bash-completion dir
 kubectl completion bash >/etc/bash_completion.d/kubectl
 
 # alias kubectl to k 
@@ -30,7 +31,7 @@ echo "alias ka='kubectl apply -f'" >> ~/.bashrc
 echo "alias kd='kubectl delete -f'" >> ~/.bashrc
 echo 'complete -F __start_kubectl k' >> ~/.bashrc
 
-# helm installed 
+# install helm 
 DESIRED_VERSION=v3.12.0
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh

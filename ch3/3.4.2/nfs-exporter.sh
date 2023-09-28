919a7bd428d35259952c6dfa3e3f14adc03cc595
Hoon Jo <pagaia@hotmail.com>
2023-09-28 12:50:41 +0900

    upt 3.4.2 pv pvc

--- ch3/3.4.2/nfs-exporter.sh
+++ ch3/3.4.2/nfs-exporter.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 nfsdir=/nfs_shared/$1
 
+# convert term in CentOS and Ubuntu distro linux  
+if [[ "$(awk -F '=' '/PRETTY_NAME/ { print $2 }' /etc/os-release)" = *"CentOS"* ]]; then 
+  NFS_SVC_NAME="nfs"
+elif [[ "$(awk -F '=' '/PRETTY_NAME/ { print $2 }' /etc/os-release)" = *"Ubuntu"* ]]; then
+  NFS_SVC_NAME="nfs-server"  
+else
+  echo "This system is not CentOS as well as Ubuntu"
+fi 
+
+# main nfs-exporter
 if [ $# -eq 0 ]; then
   echo "usage: nfs-exporter.sh <name>"; exit 0
 fi
@@ -12,8 +22,8 @@ fi
 if [[ ! -d $nfsdir ]]; then
   mkdir -p $nfsdir
   echo "$nfsdir 192.168.1.0/24(rw,sync,no_root_squash)" >> /etc/exports
-  if [[ $(systemctl is-enabled nfs) -eq "disabled" ]]; then
-    systemctl enable nfs
+  if [[ $(systemctl is-enabled "$NFS_SVC_NAME") -eq "disabled" ]]; then
+    systemctl enable $NFS_SVC_NAME
   fi
-    systemctl restart nfs
+    systemctl restart $NFS_SVC_NAME
 fi

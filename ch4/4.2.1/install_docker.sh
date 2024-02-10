d8f13e09f010ae7d8c91a83aae31ed2f9c80bf93
Hoon Jo <pagaia@hotmail.com>
2024-02-10 18:49:05 +0900

    rmv spaces

--- ch4/4.2.1/install_docker.sh
+++ ch4/4.2.1/install_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Docker version 
-docker_V = '5:24.0.6-1~ubuntu.22.04~jammy' 
+docker_V='5:24.0.6-1~ubuntu.22.04~jammy' 
 
 # install & enable docker 
 apt-get install -y docker-ce=$docker_V docker-ce-cli=$docker_V 

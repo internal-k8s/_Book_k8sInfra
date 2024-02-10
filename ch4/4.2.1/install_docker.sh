7d2000fc16de72b8d04f97aed05b324a033d8048
Hoon Jo <pagaia@hotmail.com>
2024-02-10 18:39:34 +0900

    upt install_docker.sh

---
+++ ch4/4.2.1/install_docker.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Docker version 
+docker_V = '5:24.0.6-1~ubuntu.22.04~jammy' 
+
+# install & enable docker 
+apt-get install -y docker-ce=$docker_V docker-ce-cli=$docker_V 
+systemctl enable --now docker

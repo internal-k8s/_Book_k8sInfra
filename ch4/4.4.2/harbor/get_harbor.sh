ffbeaed5753670283be478d9abde64fd950c59fd
root <root@cp-k8s>
2024-02-24 21:02:18 +0900

    modify dir structure and uninstaller

--- ch4/4.4.2/harbor/get_harbor.sh
+++ ch4/4.4.2/harbor/get_harbor.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+echo "download harbor installer..."
 curl -LO https://github.com/goharbor/harbor/releases/download/v2.10.0/harbor-online-installer-v2.10.0.tgz
 tar -xvf harbor-online-installer-v2.10.0.tgz
 mv harbor/* .
+echo "remove garbage files..."
 rm -f harbor-online-installer-v2.10.0.tgz
 rm -rf harbor

504fe6af60dd3c3478548b5b1c08550f7beab4f7
root <root@cp-k8s>
2024-02-25 09:01:37 +0900

    merge reset_certs.sh and uninstall_harbor.sh

--- ch4/4.4.2/uninstall_harbor.sh
+++ ch4/4.4.2/uninstall_harbor.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 TAG=v2.10.0
+HARBOR_HOST=192.168.1.10:8443
+DOCKER_CERT_STORE=/etc/docker/certs.d/$HARBOR_HOST
+HOST_CERT_STORE=/usr/local/share/ca-certificates
 
 echo "stop harbor..."
 docker compose -f harbor/docker-compose.yml down
 
 echo "remove harbor..."
-# preserve initial scripts
-mv ./harbor/get_harbor.sh ./
-mv ./harbor/modify_config.sh ./
-rm -rf ./harbor/*
+rm -rf ./harbor
 rm -rf /harbor-data
-mv get_harbor.sh ./harbor
-mv modify_config.sh ./harbor
 
 echo "remove harbor images..."
-docker rmi -f goharbor/redis-photon:$TAG \
+docker rmi goharbor/redis-photon:$TAG \
 goharbor/harbor-registryctl:$TAG \
 goharbor/registry-photon:$TAG \
 goharbor/nginx-photon:$TAG \
@@ -24,3 +22,26 @@ goharbor/harbor-core:$TAG \
 goharbor/harbor-portal:$TAG \
 goharbor/harbor-db:$TAG \
 goharbor/prepare:$TAG
+
+echo "remove deployed certificate..."
+for i in {1..3}
+  do
+    echo "remove certificate on w$i-k8s"
+    sshpass -p vagrant ssh -o StrictHostKeyChecking=no \
+            root@192.168.1.10$i rm -rf $DOCKER_CERT_STORE
+    sshpass -p vagrant ssh root@192.168.1.10$i \
+            rm $HOST_CERT_STORE/harbor_ca.crt
+    sshpass -p vagrant ssh root@192.168.1.10$i \
+            update-ca-certificates -f
+    sshpass -p vagrant ssh root@192.168.1.10$i \
+            systemctl restart containerd
+  done
+
+echo "remove deployed private key and certificate on control plane..."
+rm -f /harbor-data/server.* ./ca.* ./server.csr
+rm -rf $DOCKER_CERT_STORE
+rm -f $HOST_CERT_STORE/harbor_ca.crt
+
+echo "reset control plane environment..."
+update-ca-certificates -f
+systemctl restart containerd

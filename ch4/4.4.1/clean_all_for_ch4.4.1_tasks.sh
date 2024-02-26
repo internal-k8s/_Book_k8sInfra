c0c629009a98eac83a250d9ea60c2cb744834b94
Hoon Jo <pagaia@hotmail.com>
2024-02-26 15:30:25 +0900

    add purge docker-ce

--- ch4/4.4.1/clean_all_for_ch4.4.1_tasks.sh
+++ ch4/4.4.1/clean_all_for_ch4.4.1_tasks.sh
@@ -6,16 +6,19 @@ IMG_NAME="multistage-img"
 echo "[Step 1/5] Delete deployment $CRI_NAME"
 kubectl delete deployment $CRI_NAME 
 
-echo "[Step 2/5] Delete docker & containerd images in w1-k8s"
-sshpass -p vagrant ssh root@w1-k8s docker rmi $IMG_NAME > /dev/null 2>&1
-sshpass -p vagrant ssh root@w1-k8s crictl rmi $IMG_NAME > /dev/null 2>&1
+echo "[Step 2/5] Uninstall packages, Delete docker & containerd images in w1-k8s"
+sshpass -p vagrant ssh root@w1-k8s docker rmi $IMG_NAME    > /dev/null 2>&1
+sshpass -p vagrant ssh root@w1-k8s crictl rmi $IMG_NAME    > /dev/null 2>&1
+sshpass -p vagrant ssh root@w1-k8s apt-get purge docker-ce > /dev/null 2>&1
 
-echo "[Step 3/5] Delete docker & containerd images in w2-k8s"
-sshpass -p vagrant ssh root@w2-k8s docker rmi $IMG_NAME > /dev/null 2>&1
-sshpass -p vagrant ssh root@w2-k8s crictl rmi $IMG_NAME > /dev/null 2>&1
+echo "[Step 3/5] Uninstall packages, Delete docker & containerd images in w2-k8s"
+sshpass -p vagrant ssh root@w2-k8s docker rmi $IMG_NAME    > /dev/null 2>&1
+sshpass -p vagrant ssh root@w2-k8s crictl rmi $IMG_NAME    > /dev/null 2>&1
+sshpass -p vagrant ssh root@w2-k8s apt-get purge docker-ce > /dev/null 2>&1
 
-echo "[Step 4/5] Delete docker & containerd images in w3-k8s"
-sshpass -p vagrant ssh root@w3-k8s docker rmi $IMG_NAME > /dev/null 2>&1
-sshpass -p vagrant ssh root@w3-k8s crictl rmi $IMG_NAME > /dev/null 2>&1
+echo "[Step 4/5] Uninstall packages, Delete docker & containerd images in w3-k8s"
+sshpass -p vagrant ssh root@w3-k8s docker rmi $IMG_NAME    > /dev/null 2>&1
+sshpass -p vagrant ssh root@w3-k8s crictl rmi $IMG_NAME    > /dev/null 2>&1
+sshpass -p vagrant ssh root@w3-k8s apt-get purge docker-ce > /dev/null 2>&1
 
 echo "[Step 5/5] Successfully completed"

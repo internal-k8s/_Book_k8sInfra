c3a2c8050a4724250ed4c1294df8b8dfde838beb
Hoon Jo <pagaia@hotmail.com>
2024-03-01 19:04:37 +0900

    rmv ...

--- ch4/4.4.2/1.harbor_pki/1-2.deploy_certs.sh
+++ ch4/4.4.2/1.harbor_pki/1-2.deploy_certs.sh
@@ -30,7 +30,7 @@ cp ca.crt $DOCKER_CERT_DIR/ca.crt
 cp ca.crt $HOST_CERT_DIR/harbor_ca.crt
 
 echo "[Step 5/5] Copy server key and certificate to Harbor"
-mv server.key /harbor-data
-mv server.crt /harbor-data
+cp server.key /harbor-data
+cp server.crt /harbor-data
 update-ca-certificates 
 systemctl restart containerd

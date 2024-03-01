4e618f357e95a5950ca1b76598a523e29536098e
Hoon Jo <pagaia@hotmail.com>
2024-03-01 19:09:18 +0900

    rmv ...

--- ch4/4.4.2/2.harbor/2-1.get_harbor.sh
+++ ch4/4.4.2/2.harbor/2-1.get_harbor.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Download harbor-online-installer-v.2.10.0..."
+echo "Download harbor-online-installer-v.2.10.0"
 curl -LO https://github.com/goharbor/harbor/releases/download/v2.10.0/harbor-online-installer-v2.10.0.tgz
 tar -xvf harbor-online-installer-v2.10.0.tgz
 mv harbor/* .
 
-echo "Remove garbage files..."
+echo "Remove garbage files"
 rm -f harbor-online-installer-v2.10.0.tgz
 rm -rf harbor
 
@@ -15,6 +15,6 @@ echo "install.sh >>> 2-4.install.sh"
 mv prepare 2-3.prepare 
 mv install.sh 2-4.install.sh
 
-# Modify 2-4.install.sha
+# modify 2-4.install.sha
 sed -i 's/prepare $prepare_para/2-3.prepare $prepare_para/' 2-4.install.sh
 

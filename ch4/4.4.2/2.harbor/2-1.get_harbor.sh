ac8048a37ae1c140c63e9e227c7442814f0128f6
Hoon Jo <pagaia@hotmail.com>
2024-03-01 19:21:10 +0900

    add ...

--- ch4/4.4.2/2.harbor/2-1.get_harbor.sh
+++ ch4/4.4.2/2.harbor/2-1.get_harbor.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Download harbor-online-installer-v.2.10.0"
+echo "Download harbor-online-installer-v.2.10.0..."
 curl -LO https://github.com/goharbor/harbor/releases/download/v2.10.0/harbor-online-installer-v2.10.0.tgz
 tar -xvf harbor-online-installer-v2.10.0.tgz
 mv harbor/* .

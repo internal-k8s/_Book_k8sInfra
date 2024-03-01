5a870988e98a42d537875b5aeedce5adedff4cca
Hoon Jo <pagaia@hotmail.com>
2024-03-01 18:59:58 +0900

    rmv ...

--- ch4/4.4.2/1.harbor_pki/1-2.deploy_certs.sh
+++ ch4/4.4.2/1.harbor_pki/1-2.deploy_certs.sh
@@ -7,16 +7,16 @@ HOST_CERT_DIR=/usr/local/share/ca-certificates
 # if 4.4.1 skipped, it should run. so add here. 
 apt-get install sshpass -y > /dev/null 2>&1
 
-echo "[Step 1/5] Create Harbor data directory..."
+echo "[Step 1/5] Create Harbor data directory"
 mkdir -p /harbor-data
 
-echo "[Step 2/5] Create Docker certificate store..."
+echo "[Step 2/5] Create Docker certificate directory"
 mkdir -p $DOCKER_CERT_DIR
 
-echo "[Step 3/5] Deploying certificate to each of worker nodes..."
+echo "[Step 3/5] Copying certificate to each of worker nodes"
 for i in {1..3}
   do
-    echo "Deploy to node w$i-k8s"
+    echo "Copy to node w$i-k8s"
     sshpass -p vagrant scp -o StrictHostKeyChecking=no \
 	    ./ca.crt 192.168.1.10$i:$HOST_CERT_DIR/harbor_ca.crt
     sshpass -p vagrant ssh root@192.168.1.10$i \
@@ -25,11 +25,11 @@ for i in {1..3}
 	    systemctl restart containerd
   done
 
-echo "[Step 4/5] Deploy certificate to control plane..."
+echo "[Step 4/5] Copy certificate to control plane"
 cp ca.crt $DOCKER_CERT_DIR/ca.crt
 cp ca.crt $HOST_CERT_DIR/harbor_ca.crt
 
-echo "[Step 5/5] Deploy private key and certificate to Harbor..."
+echo "[Step 5/5] Copy server key and certificate to Harbor"
 mv server.key /harbor-data
 mv server.crt /harbor-data
 update-ca-certificates 

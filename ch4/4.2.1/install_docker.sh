728bafb95e99b31271d077b1b90490e2f98eec1e
Hoon Jo <pagaia@hotmail.com>
2024-03-03 14:05:12 +0900

    add docker-ce-cli due to fixed ver

--- ch4/4.2.1/install_docker.sh
+++ ch4/4.2.1/install_docker.sh
@@ -5,5 +5,5 @@ docker_V='5:24.0.6-1~ubuntu.22.04~jammy'
 
 # install & enable docker 
 apt-get update 
-apt-get install docker-ce=$docker_V -y
+apt-get install docker-ce=$docker_V docker-ce-cli=$docker_V -y 
 

28f585f2237dc88cc3494bb109f3c8c4b60e1cb1
Hoon Jo <pagaia@hotmail.com>
2024-03-05 21:12:20 +0900

    chg kube repo addr

--- ch3/3.1.3/k8s_env_build.sh
+++ ch3/3.1.3/k8s_env_build.sh
@@ -8,15 +8,11 @@ swapoff -a
 # sed to comment the swap partition in /etc/fstab (Rmv blank)
 sed -i.bak -r 's/(.+swap.+)/#\1/' /etc/fstab
 
-# add kubernetes repo ONLY for 22.04
-mkdir -p /etc/apt/keyrings
-curl -fsSL \
-  https://packages.cloud.google.com/apt/doc/apt-key.gpg \
-  | gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
-echo \
-  "deb [signed-by=/etc/apt/keyrings/kubernetes.gpg] \
-  https://apt.kubernetes.io/ kubernetes-xenial main" \
-  | tee /etc/apt/sources.list.d/kubernetes.list
+# add kubernetes repo 
+curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+EOF
 
 # add docker-ce repo with containerd
 apt-get update && apt-get install gnupg lsb-release

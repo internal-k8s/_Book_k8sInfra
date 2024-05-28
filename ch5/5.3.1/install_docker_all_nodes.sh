46a85961fba9251bcc7fd0b902aff3a7213e1a1f
root <root@cp-k8s>
2024-05-28 20:09:20 +0900

    ch5

--- ch5/5.3.1/install_docker_all_nodes.sh
+++ ch5/5.3.1/install_docker_all_nodes.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 NODE_COUNT=3
-echo "[Step 1/4] Install necessary packages"
+echo "[Step 1/3] Install necessary packages"
 apt-get install sshpass -y > /dev/null 2>&1
 
-echo "[Step 2/3] Install docker and copy kubeconfig to all nodes"
+echo "[Step 2/3] Install docker to all nodes"
 for (( i=1; i<=$NODE_COUNT; i++ ));
 do
 TARGET="192.168.1.10$i"
 echo "working on $TARGET"
 sshpass -p vagrant scp -o StrictHostKeyChecking=no -q ~/_Book_k8sInfra/ch4/4.2.1/install_docker.sh root@$TARGET:/tmp
 sshpass -p vagrant ssh root@$TARGET bash /tmp/install_docker.sh > /dev/null 2>&1
+sshpass -p vagrant ssh root@$TARGET chmod 666 /var/run/docker.sock 2>&1
 done
 echo "[Step 3/3] Successfully completed"

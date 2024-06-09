ce28d14a2bd7a625fa510e080c45866f68cfcfde
Gnu Shim <geunwoo.j.shim@gmail.com>
2024-06-09 13:29:06 +0900

    unnecessary sshpass remove

--- ch5/5.3.1/install_docker_all_nodes.sh
+++ ch5/5.3.1/install_docker_all_nodes.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 NODE_COUNT=3
-echo "[Step 1/3] Install necessary packages"
-apt-get install sshpass -y > /dev/null 2>&1
-
-echo "[Step 2/3] Install docker to all nodes"
+echo "[Step 1/2] Install docker to all nodes"
 for (( i=1; i<=$NODE_COUNT; i++ ));
 do
 TARGET="192.168.1.10$i"
@@ -12,4 +9,4 @@ sshpass -p vagrant scp -o StrictHostKeyChecking=no -q ~/_Book_k8sInfra/ch4/4.2.1
 sshpass -p vagrant ssh root@$TARGET bash /tmp/install_docker.sh > /dev/null 2>&1
 sshpass -p vagrant ssh root@$TARGET chmod 666 /var/run/docker.sock 2>&1
 done
-echo "[Step 3/3] Successfully completed"
+echo "[Step 2/3] Successfully completed"

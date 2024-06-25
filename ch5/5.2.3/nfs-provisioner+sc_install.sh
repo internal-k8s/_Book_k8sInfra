e0c3f88d004beed6c2eb97307113475d96128bd1
Hoon Jo <pagaia@hotmail.com>
2024-06-25 10:55:06 +0900

    upt 5.2.3

--- ch5/5.2.3/nfs-provisioner+sc_install.sh
+++ ch5/5.2.3/nfs-provisioner+sc_install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-helm install nfs-subdir-external-provisioner book-k8sinfra-v2/nfs-subdir-external-provisioner \
+
+helm install nfs-provisioner+sc edu/nfs-subdir-external-provisioner \
 --namespace nfs-provisioner \
 --create-namespace \
 --set nfs.server='192.168.1.10' \
 --set nfs.path='/nfs_shared/dynamic-vol' \
 --set storageClass.name='managed-nfs-storage' \
 --set storageClass.pathPattern='${.PVC.namespace}-${.PVC.name}' \
---set storageClass.accessModes='ReadWriteMany' \
+--set storageClass.provisionerName='k8s-sigs.io/nfs-subdir-external-provisioner' \
 --set storageClass.onDelete='delete'
-
 

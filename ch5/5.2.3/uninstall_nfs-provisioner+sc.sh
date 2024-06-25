c43bd19a73878c3d2409d1c0532231164637828e
Hoon Jo <pagaia@hotmail.com>
2024-06-25 10:56:17 +0900

    upt 5.2.3

---
+++ ch5/5.2.3/uninstall_nfs-provisioner+sc.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+kubectl delete -f ~/_Book_k8sInfra/ch5/5.2.2/FROM_3.4.3/storageclass.yaml
+kubectl delete -f ~/_Book_k8sInfra/ch5/5.2.2/FROM_3.4.3/nfs-subdir-external-provisioner-v4.0.0.yaml
+
+

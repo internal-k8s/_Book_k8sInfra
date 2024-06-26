77c4d7b9cbaade48aded607eaf818c5746ab21a9
Hoon Jo <pagaia@hotmail.com>
2024-06-26 14:08:04 +0900

    chg comment

--- ch5/5.3.1/install_docker_on_all_nodes.sh
+++ ch5/5.3.1/install_docker_on_all_nodes.sh
@@ -6,7 +6,7 @@ echo "[Step 1/2] Install docker service on all nodes"
 for (( i=1; i<=$NODE_COUNT; i++ ));
   do
   TARGET="192.168.1.10$i"
-  echo "  - Installing docker on $TARGET"
+  echo "  - installing docker on $TARGET"
   sshpass -p vagrant scp -o StrictHostKeyChecking=no -q ~/_Book_k8sInfra/ch4/4.2.1/install_docker.sh root@$TARGET:/tmp
   sshpass -p vagrant ssh root@$TARGET bash /tmp/install_docker.sh > /dev/null 2>&1
   sshpass -p vagrant ssh root@$TARGET chmod 666 /var/run/docker.sock 2>&1

c6660ce34271669bbffddd4ada1c05fa0f94492f
Seongju <seongju.moon@linux.com>
2024-08-16 08:16:29 +0900

    operator: cilium lab.

--- app/D.operator/1.cilium/1.nginx-installer.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO: chk-hn 써보는게 어떨까?
-kubectl create deploy nginx --image nginx:stable
-kubectl expose deploy nginx --name nginx --type LoadBalancer --port 80 --target-port 80
--- app/D.operator/1.cilium/2.nginx-uninstaller.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO: chk-hn 써보는게 어떨까?
-kubectl delete deploy nginx
-kubectl delete svc nginx 

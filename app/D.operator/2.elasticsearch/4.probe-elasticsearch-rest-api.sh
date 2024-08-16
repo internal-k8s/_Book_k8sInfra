efbbca2e813b5eb0c3916228bcf0882c7e553a11
Seongju <seongju.moon@linux.com>
2024-08-16 09:19:32 +0900

    WIP: operator renewal.

--- app/D.operator/2.elasticsearch/4.probe-elasticsearch-rest-api.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl exec -it elasticsearch-es-default-0 -- curl -k https://elasticsearch-es-default-0.elasticsearch-es-default.default.svc.cluster.local:9200 -u elastic:$1

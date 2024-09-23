9bf52ca5f76f6c54a5d073793337f4628fae0a79
Seongju Mun <seongju.moon@linux.com>
2024-09-23 23:31:02 +0900

    rename file capitalize.

--- app/.D.Operator/2.elasticsearch/2.summaries_elasticsearch_stat.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-KB_ENDPOINT="http://$(kubectl get svc kibana-kb-http -o jsonpath='${.status.loadBalancer.ingress[0].ip}'):5601"
-KB_PASSWORD="$(kubectl get secret elasticsearch-es-elastic-user -o jsonpath='{ .data.elastic }' | base64 -d)"
-
-echo -e "> 키바나 접속 엔드포인트: $KB_ENDPOINT\n> 키바나 접속 계정: elastic\n> 키바나 접속 패스워드: $KB_PASSWORD"

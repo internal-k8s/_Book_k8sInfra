256186a3044da2a301439f5b3d120f61a7a37d29
Seongju Mun <seongju.moon@linux.com>
2024-09-28 00:18:15 +0900

    fix suffix path operator.

--- app/D.Operator/2.elasticsearch/1.setup_elasticsearch+operator.sh
+++ app/D.Operator/2.elasticsearch/1.setup_elasticsearch+operator.sh
@@ -3,7 +3,7 @@
 echo "> 커스텀 리소스 데피니션(customresourcedefinition) 배포"
 kubectl create -f ~/_Book_k8sInfra/app/D.Operator/2.elasticsearch/elastic-operator-config/crds.yaml
 echo "> 엘라스틱서치 오퍼레이터(elastic-Operator) 배포"
-kubectl create -f ~/_Book_k8sInfra/app/D.Operator/2.elasticsearch/elastic-operator-config/Operator.yaml
+kubectl create -f ~/_Book_k8sInfra/app/D.Operator/2.elasticsearch/elastic-operator-config/operator.yaml
 echo "> 로그 적재를 위한 엘라스틱서치(elasitcsearch) 클러스터 배포"
 kubectl create -f ~/_Book_k8sInfra/app/D.Operator/2.elasticsearch/elasticsearch.yaml
 echo "> 엘라스틱서치에 적재된 로그를 시각화하기 위한 키바나(kibana) 배포"

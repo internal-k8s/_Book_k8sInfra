aee5ad5f1de437478fe1a7890f696ba800b1eacd
Seongju Mun <seongju.moon@linux.com>
2024-09-28 00:06:46 +0900

    fix crd spec ensure cilium 1.16 and fix directory name.

--- app/D.Operator/1.cilium/1.setup_cilium_loadbalancer.sh
+++ app/D.Operator/1.cilium/1.setup_cilium_loadbalancer.sh
@@ -3,16 +3,16 @@
 echo "> 실리움 로드밸런서 기능 활성화 시작"
 
 echo "> 배포 전 로드밸런서 IP 주소 영역 확인 "
-cat ~/_Book_k8sInfra/app/D.operator/1.cilium/cilium-lb-config/cilium-loadbalancer-ip-pool.yaml | yq .spec
+cat ~/_Book_k8sInfra/app/D.Operator/1.cilium/cilium-lb-config/cilium-loadbalancer-ip-pool.yaml | yq .spec
 echo "> 로드밸런서 IP 주소 영역 설정"
-kubectl apply -f ~/_Book_k8sInfra/app/D.operator/1.cilium/cilium-lb-config/cilium-loadbalancer-ip-pool.yaml
+kubectl apply -f ~/_Book_k8sInfra/app/D.Operator/1.cilium/cilium-lb-config/cilium-loadbalancer-ip-pool.yaml
 echo "> 배포 후 로드밸런서 IP 주소 영역 확인 "
 kubectl get ciliumloadbalancerippools.cilium.io lb-address-pool -o yaml | yq .spec
 
 echo "> 배포 전 로드밸런서 IP 주소 영역 확인 "
-cat ~/_Book_k8sInfra/app/D.operator/1.cilium/cilium-lb-config/cilium-l2announcement-policy.yaml | yq .spec
+cat ~/_Book_k8sInfra/app/D.Operator/1.cilium/cilium-lb-config/cilium-l2announcement-policy.yaml | yq .spec
 echo "> 로드밸런서 IP 주소 영역 설정"
-kubectl apply -f ~/_Book_k8sInfra/app/D.operator/1.cilium/cilium-lb-config/cilium-l2announcement-policy.yaml
+kubectl apply -f ~/_Book_k8sInfra/app/D.Operator/1.cilium/cilium-lb-config/cilium-l2announcement-policy.yaml
 echo "> 배포 후 로드밸런서 IP 주소 영역 확인 "
 kubectl get ciliuml2announcementpolicies.cilium.io l2-loadbalancer-announce-policy -o yaml | yq .spec
 

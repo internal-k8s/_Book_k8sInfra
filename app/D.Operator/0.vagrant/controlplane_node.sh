122392c4864e55188832c307397f36eb23cafe9a
Seongju Mun <seongju.moon@linux.com>
2024-09-28 00:05:45 +0900

    update cilium version.

--- app/D.Operator/0.vagrant/controlplane_node.sh
+++ app/D.Operator/0.vagrant/controlplane_node.sh
@@ -10,22 +10,26 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-# CNI raw address & config for kubernetes's network 
-curl -L https://github.com/sysnet4admin/BB/raw/main/cilium-cli/v0.14.6/cilium \
-     -o /usr/local/bin/cilium
-chmod 744 /usr/local/bin/cilium 
+# TODO: need integration BB/raw CNI raw address & config for kubernetes's network 
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+CLI_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
 # config for kubernetes's network by cilium 
 cilium install \
-  --version=v1.14.5 \
-    --helm-set ipam.mode=kubernetes \
-    --helm-set ipv4NativeRoutingCIDR="172.16.0.0/16" \
-    --helm-set l2announcements.enabled="true" \
-    --helm-set kubeProxyReplacement="true" \
-    --helm-set externalIPs="true" \
-    --helm-set hubble.enable="true"
+  --version=v1.16.2 \
+  --helm-set ipam.mode=kubernetes \
+  --helm-set ipv4NativeRoutingCIDR="172.16.0.0/16" \
+  --helm-set l2announcements.enabled="true" \
+  --helm-set kubeProxyReplacement="true" \
+  --helm-set externalIPs.enabled="true" \
+  --helm-set hubble.enable="true"
 
 # kubectl completion on bash-completion dir
-kubectl completion bash >/etc/bash_completion.d/kubectl
+kubectl completion bash > /etc/bash_completion.d/kubectl
 
 # alias kubectl to k 
 echo 'alias k=kubectl'               >> ~/.bashrc
@@ -34,11 +38,6 @@ echo "alias kg-po-ip-stat-no='kubectl get pods -o=custom-columns=\
 NAME:.metadata.name,IP:.status.podIP,STATUS:.status.phase,NODE:.spec.nodeName'" \
                                      >> ~/.bashrc 
 echo 'complete -F __start_kubectl k' >> ~/.bashrc
-
-# Setup nfs-provisioner for elasticsearch lab
-sh -c "$HOME/_Book_k8sInfra/ch3/3.4.3/nfs_exporter.sh dynamic-vol"
-kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/nfs-subdir-external-provisioner-v4.0.0.yaml
-kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/storageclass.yaml
 
 # git clone book source 
 git clone https://github.com/internal-k8s/_Book_k8sInfra.git 

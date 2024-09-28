2814ba65c02c32294158f9605c707a68fc9c74c1
root <root@cp-k8s>
2024-09-28 18:24:21 +0900

    network naming change

--- app/E.DeepDiveContainer/ns-create.sh
+++ app/E.DeepDiveContainer/ns-create.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 apt-get install -y bridge-utils
-brctl addbr nginx
-ip link set nginx up
-ip addr add 192.168.200.1/24 dev nginx
-ip link add name vhost type veth peer name container
+brctl addbr br-nginx
+ip link set br-nginx up
+ip addr add 192.168.200.1/24 dev br-nginx
+ip link add name h-int type veth peer name c-int
 ip netns add ns-nginx
-ip link set container netns ns-nginx
-ip netns exec ns-nginx ip link set container name eth1
+ip link set c-int netns ns-nginx
+ip netns exec ns-nginx ip link set c-int name eth1
 ip netns exec ns-nginx ip addr add 192.168.200.2/24 dev eth1
 ip netns exec ns-nginx ip link set eth1 up
 ip netns exec ns-nginx ip route add default via 192.168.200.1
-ip link set vhost up
-brctl addif nginx vhost
+ip link set h-int up
+brctl addif br-nginx h-int

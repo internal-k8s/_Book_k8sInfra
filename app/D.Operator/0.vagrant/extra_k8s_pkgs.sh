1cdd909eacf92eb553106fe97bd60f1535d5ed41
Seongju Mun <seongju.moon@linux.com>
2024-10-02 14:48:12 +0900

    add extra package.

--- app/D.Operator/0.vagrant/extra_k8s_pkgs.sh
+++ app/D.Operator/0.vagrant/extra_k8s_pkgs.sh
@@ -4,3 +4,7 @@
 sh -c "$HOME/_Book_k8sInfra/ch3/3.4.3/nfs_exporter.sh dynamic-vol"
 kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/nfs-subdir-external-provisioner-v4.0.0.yaml
 kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/storageclass.yaml
+kubectl annotate storageclass managed-nfs-storage storageclass.kubernetes.io/is-default-class=true
+# Setup loadbalancer
+kubectl apply -f $HOME/_Book_k8sInfra/app/D.Operator/0.vagrant/extra-k8s-packages/cilium-l2announcement-policy.yaml
+kubectl apply -f $HOME/_Book_k8sInfra/app/D.Operator/0.vagrant/extra-k8s-packages/cilium-loadbalancer-ip-pool.yaml

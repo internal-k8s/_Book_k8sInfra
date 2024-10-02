004d8e697b740e2b4074bd95efec0ee778f6fbe6
Seongju Mun <seongju.moon@linux.com>
2024-10-02 15:04:17 +0900

    update extra loadbalancer config async apply.

--- app/D.Operator/0.vagrant/extra_k8s_pkgs.sh
+++ app/D.Operator/0.vagrant/extra_k8s_pkgs.sh
@@ -6,5 +6,5 @@ kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/nfs-subdir-external-provisioner
 kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/storageclass.yaml
 kubectl annotate storageclass managed-nfs-storage storageclass.kubernetes.io/is-default-class=true
 # Setup loadbalancer
-kubectl apply -f $HOME/_Book_k8sInfra/app/D.Operator/0.vagrant/extra-k8s-packages/cilium-l2announcement-policy.yaml
-kubectl apply -f $HOME/_Book_k8sInfra/app/D.Operator/0.vagrant/extra-k8s-packages/cilium-loadbalancer-ip-pool.yaml
+(sleep 540 && kubectl apply -f $HOME/_Book_k8sInfra/app/D.Operator/0.vagrant/extra-k8s-packages/cilium-l2announcement-policy.yaml)
+(sleep 560 && kubectl apply -f $HOME/_Book_k8sInfra/app/D.Operator/0.vagrant/extra-k8s-packages/cilium-loadbalancer-ip-pool.yaml)

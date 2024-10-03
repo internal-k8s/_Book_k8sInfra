aff21135480bf26ab4a388dab1b2002977342a82
Seongju Mun <seongju.moon@linux.com>
2024-10-03 17:28:33 +0900

    remove script improvement.

--- app/D.Operator/2.elasticsearch/5.cleanup_elasticsearch_w_operator.sh
+++ app/D.Operator/2.elasticsearch/5.cleanup_elasticsearch_w_operator.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-echo "> 엘라스틱서치에 로그를 전송하기 위한 파일비트(filebeat) 삭제"
+echo "> 엘라스틱서치에 로그를 전송하기 위한 파일비트(filebeat) 커스텀 리소스 삭제"
 kubectl delete -f ~/_Book_k8sInfra/app/D.Operator/2.elasticsearch/filebeat.yaml
 echo "> 엘라스틱서치에 적재된 로그를 시각화하기 위한 키바나(kibana) 커스텀 리소스 삭제"
 kubectl delete -f ~/_Book_k8sInfra/app/D.Operator/2.elasticsearch/kibana.yaml
@@ -11,3 +11,5 @@ echo "> 엘라스틱서치 오퍼레이터(elastic-operator) 삭제"
 kubectl delete -f ~/_Book_k8sInfra/app/D.Operator/2.elasticsearch/elastic-operator-config/operator.yaml
 echo "> 커스텀 리소스 데피니션(customresourcedefinition) 삭제"
 kubectl delete -f ~/_Book_k8sInfra/app/D.Operator/2.elasticsearch/elastic-operator-config/crds.yaml
+echo "> 엘라스틱서치 로그 데이터가 담긴 퍼시스턴스볼륨클레임 삭제"
+kubectl delete pvc elasticsearch-data-elasticsearch-es-default-0

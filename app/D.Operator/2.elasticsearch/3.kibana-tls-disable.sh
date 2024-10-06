34616f0b8e91af72b8d75ba695f59f084253f2fa
Seongju Mun <seongju.moon@linux.com>
2024-10-06 15:42:35 +0900

    apply -> replace.

--- app/D.Operator/2.elasticsearch/3.kibana-tls-disable.sh
+++ app/D.Operator/2.elasticsearch/3.kibana-tls-disable.sh
@@ -11,7 +11,7 @@ echo "===================================================="
 echo "키바나 HTTP 설정 변경 사항 확인"
 diff ~/_Book_k8sInfra/app/D.Operator/0.vagrant/extra-k8s-packages/elasticsearch/kibana.yaml kibana_tls_disable.yaml
 echo "키바나 TLS 설정 변경 적용"
-kubectl apply -f kibana_tls_disable.yaml -n elastic-system
+kubectl replace -f kibana_tls_disable.yaml -n elastic-system
 echo "===================================================="
 echo "현재 키바나 커스텀 리소스 상태"
 kubectl get kibana kibana -n elastic-system

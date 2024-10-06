a01c4a5c43ac1a734356ae03894aa04fcb1c0e88
Seongju Mun <seongju.moon@linux.com>
2024-10-06 12:58:37 +0900

    Refactor code.

--- app/D.Operator/2.elasticsearch/1.fault-inject-elasticsearch.sh
+++ app/D.Operator/2.elasticsearch/1.fault-inject-elasticsearch.sh
@@ -3,34 +3,34 @@
 
 echo "===================================================="
 echo "현재 엘라스틱서치 커스텀 리소스 상태"
-kubectl get elasticsearch elasticsearch
+kubectl get elasticsearch elasticsearch -n elastic-system
 echo "===================================================="
 echo "현재 엘라스틱서치 스테이트풀셋 상태"
-kubectl get statefulset -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch
+kubectl get statefulset -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch -n elastic-system
 echo "===================================================="
 echo "현재 엘라스틱서치 파드 상태"
-kubectl get pods -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch
+kubectl get pods -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch -n elastic-system
 echo "===================================================="
 echo "결함 주입: 스테이트풀셋 삭제"
-kubectl delete statefulset -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch
+kubectl delete statefulset -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch -n elastic-system
 sleep 10
 echo "===================================================="
 echo "현재 엘라스틱서치 커스텀 리소스 상태"
-kubectl get elasticsearch elasticsearch
+kubectl get elasticsearch elasticsearch -n elastic-system
 echo "===================================================="
 echo "현재 엘라스틱서치 스테이트풀셋 상태"
-kubectl get statefulset -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch
+kubectl get statefulset -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch -n elastic-system
 echo "===================================================="
 echo "현재 가용 가능한 엘라스틱서치 퍼시스턴스볼륨클레임"
-kubectl get pvc -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch
+kubectl get pvc -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch -n elastic-system
 echo "===================================================="
 echo "현재 엘라스틱서치 파드 상태"
-kubectl get pods -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch
+kubectl get pods -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch -n elastic-system
 echo "엘라스틱서치가 사용 가능할 때 까지 대기"
-while [[ $(kubectl get elasticsearch elasticsearch -n default -o jsonpath='{.status.phase}') != "Ready" ]]; do
+while [[ $(kubectl get elasticsearch elasticsearch -n elastic-system -o jsonpath='{.status.phase}') != "Ready" ]]; do
   echo "Waiting for Elasticsearch to be in Ready phase..."
   sleep 10
 done
 echo "Elasticsearch is now Ready."
-kubectl get pods -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch
+kubectl get pods -l elasticsearch.k8s.elastic.co/cluster-name=elasticsearch -n elastic-system
 echo "===================================================="
--- app/D.Operator/2.elasticsearch/2.print_kibana_conn_info.sh
+++ app/D.Operator/2.elasticsearch/2.print_kibana_conn_info.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-KB_ENDPOINT="http://$(kubectl get svc kibana-kb-http -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
-KB_PASSWORD="$(kubectl get secret elasticsearch-es-elastic-user -o jsonpath='{ .data.elastic }' | base64 -d)"
+KB_ENDPOINT="http://$(kubectl get svc kibana-kb-http -n elastic-system -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
+KB_PASSWORD="$(kubectl get secret elasticsearch-es-elastic-user -n elastic-system -o jsonpath='{ .data.elastic }' | base64 -d)"
 
 echo -e "> 키바나 접속 엔드포인트: $KB_ENDPOINT\n> 키바나 접속 계정: elastic\n> 키바나 접속 패스워드: $KB_PASSWORD"

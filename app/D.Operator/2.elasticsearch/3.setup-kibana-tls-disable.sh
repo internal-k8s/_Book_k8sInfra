5b0ae7704ed3cccfbf757f1b267bca20c89d4367
Seongju Mun <seongju.moon@linux.com>
2024-10-06 12:58:37 +0900

    Refactor code.

--- app/D.Operator/2.elasticsearch/3.setup-kibana-tls-disable.sh
+++ app/D.Operator/2.elasticsearch/3.setup-kibana-tls-disable.sh
@@ -3,25 +3,25 @@
 
 echo "===================================================="
 echo "현재 키바나 커스텀 리소스 상태"
-kubectl get kibana kibana
+kubectl get kibana kibana -n elastic-system
 echo "===================================================="
 echo "현재 키바나 파드 상태"
-kubectl get pods -l kibana.k8s.elastic.co/name=kibana
+kubectl get pods -l kibana.k8s.elastic.co/name=kibana -n elastic-system
 echo "===================================================="
 echo "키바나 HTTP 설정 변경 사항 확인"
-diff kibana.yaml kibana_tls_disable.yaml
+diff ~/_Book_k8sInfra/app/D.Operator/0.vagrant/extra-k8s-packages/elasticsearch/kibana.yaml kibana_tls_disable.yaml
 echo "키바나 TLS 설정 변경 적용"
-kubectl apply -f kibana_tls_disable.yaml
+kubectl apply -f kibana_tls_disable.yaml -n elastic-system
 echo "===================================================="
 echo "현재 키바나 커스텀 리소스 상태"
-kubectl get kibana kibana
+kubectl get kibana kibana -n elastic-system
 echo "===================================================="
 echo "현재 키바나 파드 상태"
-kubectl get pods -l kibana.k8s.elastic.co/name=kibana
+kubectl get pods -l kibana.k8s.elastic.co/name=kibana -n elastic-system
 echo "===================================================="
 echo "새로 배포되는 키바나가 준비될 때까지 대기"
-kubectl wait --for=condition=ready pod -l kibana.k8s.elastic.co/name=kibana --timeout=300s
+kubectl wait --for=condition=ready pod -l kibana.k8s.elastic.co/name=kibana --timeout=300s -n elastic-system
 echo "===================================================="
 echo "현재 키바나 파드 상태"
-kubectl get pods -l kibana.k8s.elastic.co/name=kibana
+kubectl get pods -l kibana.k8s.elastic.co/name=kibana -n elastic-system
 echo "===================================================="

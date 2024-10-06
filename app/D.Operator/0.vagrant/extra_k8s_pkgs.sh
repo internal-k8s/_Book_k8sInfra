83f8940244b677ffd8d01200e6668af0c5d83d09
Seongju Mun <seongju.moon@linux.com>
2024-10-06 10:37:32 +0900

    refactor.

--- app/D.Operator/0.vagrant/extra_k8s_pkgs.sh
+++ app/D.Operator/0.vagrant/extra_k8s_pkgs.sh
@@ -6,5 +6,7 @@ kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/nfs-subdir-external-provisioner
 kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/storageclass.yaml
 kubectl annotate storageclass managed-nfs-storage storageclass.kubernetes.io/is-default-class=true
 # Setup loadbalancer
-(sleep 540 && kubectl apply -f $HOME/_Book_k8sInfra/app/D.Operator/0.vagrant/extra-k8s-packages/cilium-l2announcement-policy.yaml)&
-(sleep 560 && kubectl apply -f $HOME/_Book_k8sInfra/app/D.Operator/0.vagrant/extra-k8s-packages/cilium-loadbalancer-ip-pool.yaml)&
+(sleep 540 && kubectl apply -f $HOME/_Book_k8sInfra/app/D.Operator/0.vagrant/extra-k8s-packages/cilium/cilium-l2announcement-policy.yaml)&
+(sleep 560 && kubectl apply -f $HOME/_Book_k8sInfra/app/D.Operator/0.vagrant/extra-k8s-packages/cilium/cilium-loadbalancer-ip-pool.yaml)&
+# Setup elasticsearch
+sh -c "$HOME/_Book_k8sInfra/app/D.Operator/0.vagrant/extra-k8s-packages/elasticsearch/setup_elasticsearch_w_operator.sh"

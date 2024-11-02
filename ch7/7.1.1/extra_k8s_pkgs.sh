9c658532e7eb9349f160bfef0ed4c9bb7584bf71
Seongju Mun <seongju.moon@linux.com>
2024-11-02 17:04:30 +0900

    ready for deploy ch7

---
+++ ch7/7.1.1/extra_k8s_pkgs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Setup nfs-provisioner for elasticsearch lab
+sh -c "$HOME/_Book_k8sInfra/ch3/3.4.3/nfs_exporter.sh dynamic-vol"
+kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/nfs-subdir-external-provisioner-v4.0.0.yaml
+kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/storageclass.yaml
+kubectl annotate storageclass managed-nfs-storage storageclass.kubernetes.io/is-default-class=true
+# Setup loadbalancer
+(sleep 540 && kubectl apply -f $HOME/_Book_k8sInfra/ch7/7.1.1/extra-k8s-packages/cilium/cilium-l2announcement-policy.yaml)&
+(sleep 560 && kubectl apply -f $HOME/_Book_k8sInfra/ch7/7.1.1/extra-k8s-packages/cilium/cilium-loadbalancer-ip-pool.yaml)&

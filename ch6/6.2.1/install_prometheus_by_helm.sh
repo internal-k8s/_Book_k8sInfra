561f38431f3d594a74ab9b5eafc5d5e89a98da6e
Seongju Mun <seongju.moon@linux.com>
2024-12-23 15:47:55 +0900

    push all green [wip] install prometheus 3.0.1

---
+++ ch6/6.2.1/install_prometheus_by_helm.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+helm install prometheus edu/prometheus \
+--namespace monitoring \
+--create-namespace \
+--set prometheus-pushgateway.enabled=false \
+--set alertmanager.enabled=false \
+--set nodeExporter.tolerations[0].key=node-role.kubernetes.io/control-plane \
+--set nodeExporter.tolerations[0].effect=NoSchedule \
+--set nodeExporter.tolerations[0].operator=Exist \
+--set server.securityContext.runAsGroup=1000 \
+--set server.securityContext.runAsUser=1000 \
+--set server.securityContext.runAsNonRoot=false \
+--set server.statefulSet.enabled=true \
+--set server.persistentVolume.storageClass="managed-nfs-storage" \
+--set server.service.type="LoadBalancer" \
+--set server.extraFlags[0]="storage.tsdb.no-lockfile" 

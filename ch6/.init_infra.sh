b26ad55ded08f1735ef25da7a8743358b50bacb4
Hoon Jo <pagaia@hotmail.com>
2024-12-29 14:43:25 +0900

    upt init_infra.sh

--- ch6/.init_infra.sh
+++ ch6/.init_infra.sh
@@ -10,7 +10,8 @@ kubectl apply -f ~/_Book_k8sInfra/ch3/3.4.3/nfs-subdir-external-provisioner-v4.0
 kubectl apply -f ~/_Book_k8sInfra/ch3/3.4.3/storageclass.yaml
 bash ~/_Book_k8sInfra/ch3/3.4.3/nfs_exporter.sh "dynamic-vol"
 
-# Helm 
+# Helm & repo add 
 bash ~/_Book_k8sInfra/ch5/5.2.3/install_helm.sh
 bash ~/_Book_k8sInfra/ch5/5.2.3/helm_completion.sh
+helm repo add edu https://k8s-edu.github.io/Bkv2_main/helm-charts
 

cdd89ab0eddff464f05890df2c541c4eae8561b2
Hoon Jo <pagaia@hotmail.com>
2024-12-29 14:31:22 +0900

    upt init_infra.sh

---
+++ ch6/.init_infra.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# LoadBalancer 
+kubectl apply -f ~/_Book_k8sInfra/ch3/3.3.2/metallb-native-v0.13.10.yaml
+echo "Waiting 60secs for MetalLB CRD" ; sleep 60
+kubectl apply -f ~/_Book_k8sInfra/ch3/3.3.2/metallb-l2-iprange.yaml
+
+# StorageClass  
+kubectl apply -f ~/_Book_k8sInfra/ch3/3.4.3/nfs-subdir-external-provisioner-v4.0.0.yaml
+kubectl apply -f ~/_Book_k8sInfra/ch3/3.4.3/storageclass.yaml
+bash ~/_Book_k8sInfra/ch3/3.4.3/nfs_exporter.sh "dynamic-vol"
+
+# Helm 
+bash ~/_Book_k8sInfra/ch5/5.2.3/install_helm.sh
+bash ~/_Book_k8sInfra/ch5/5.2.3/helm_completion.sh
+

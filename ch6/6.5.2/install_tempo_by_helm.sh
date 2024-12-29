5c97b8a8e90b8b05ca8647289866e28b1b1ca398
Seongju Mun <seongju.moon@linux.com>
2024-12-29 14:33:22 +0900

    fix naming remove topologyconstraint.

--- ch6/6.5.2/install_tempo_by_helm.sh
+++ ch6/6.5.2/install_tempo_by_helm.sh
@@ -6,8 +6,4 @@ helm install tempo edu/tempo \
 --create-namespace \
 --set persistence.enabled=true \
 --set persistence.storageClassName="managed-nfs-storage" \
---set nodeSelector."kubernetes\.io/hostname"=cp-k8s \
---set securityContext.runAsNonRoot=false \
---set tolerations[0].key=node-role.kubernetes.io/control-plane \
---set tolerations[0].effect=NoSchedule \
---set tolerations[0].operator=Exists
+--set securityContext.runAsNonRoot=false 

401b1a61bcafb4c1f5b7dfa6692098fb010e9f84
Hoon Jo <pagaia@hotmail.com>
2025-01-11 10:18:28 +0900

    upt 7.1.1

--- ch7/7.1.1/k8s_extra_pkgs.sh
+++ ch7/7.1.1/k8s_extra_pkgs.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-# Setup nfs-provisioner for ch7
+# deploy nfs-provisioner & storageclass as default 
 sh -c "$HOME/_Book_k8sInfra/ch3/3.4.3/nfs_exporter.sh dynamic-vol"
 kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/nfs-subdir-external-provisioner-v4.0.0.yaml
 kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/storageclass.yaml
 kubectl annotate storageclass managed-nfs-storage storageclass.kubernetes.io/is-default-class=true
-# Setup loadbalancer
-(sleep 540 && kubectl apply -f $HOME/_Book_k8sInfra/ch7/7.1.1/extra-k8s-packages/cilium/cilium-l2announcement-policy.yaml)&
-(sleep 560 && kubectl apply -f $HOME/_Book_k8sInfra/ch7/7.1.1/extra-k8s-packages/cilium/cilium-loadbalancer-ip-pool.yaml)&
+
+# config cilium layer2 mode 
+(sleep 600 && kubectl apply -f $HOME/_Book_k8sInfra/ch7/7.1.1/k8s-extra-pkgs/cilium-l2-ippool.yaml)&
+
+# install helm & add repo 
 $HOME/_Book_k8sInfra/ch5/5.2.3/install_helm.sh
 helm repo add edu https://k8s-edu.github.io/Bkv2_main/helm-charts/

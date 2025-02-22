9b33b158610a486e14d2a67a0e1ce10b7fe4c756
Seongju(Martin) Mun <seongju.moon@linux.com>
2025-02-22 13:09:46 +0900

    fix
    reorder --set param

--- ch6/6.4.1/install_grafana_by_helm.sh
+++ ch6/6.4.1/install_grafana_by_helm.sh
@@ -2,10 +2,10 @@
 helm install grafana edu/grafana \
 --namespace monitoring \
 --create-namespace \
+--set securityContext.runAsUser=1000 \
+--set securityContext.runAsGroup=1000 \
 --set persistence.enabled=true \
 --set persistence.storageClassName="managed-nfs-storage" \
 --set service.type=LoadBalancer \
---set securityContext.runAsUser=1000 \
---set securityContext.runAsGroup=1000 \
 --set "grafana\.ini".server.root_url="http://192.168.1.12" \
 --set adminPassword="admin"

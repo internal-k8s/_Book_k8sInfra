d52d5437fc40c0b19304a79016fdb83dd0de2aa1
Seongju(Martin) Mun <seongju.moon@linux.com>
2025-04-15 19:58:35 +0900

    Update install_loki_w_fluentbit_by_helm.sh

--- ch6/6.6.1/install_loki_w_fluentbit_by_helm.sh
+++ ch6/6.6.1/install_loki_w_fluentbit_by_helm.sh
@@ -8,7 +8,7 @@ helm install grafana-stack edu/grafana-stack \
 --set loki.singleBinary.persistence.enableStatefulSetAutoDeletePVC=false \
 --set loki.singleBinary.persistence.storageClass="managed-nfs-storage"
 --set fluent-bit.enabled=true \
---set fluent-bit.loki.host=loki-gateway.monitoring.svc.cluster.local \
+--set fluent-bit.loki.host=grafana-stack-loki-gateway.monitoring.svc.cluster.local/\
 --set flient-bit.tolerations[0].key=node-role.kubernetes.io/control-plane \
 --set flient-bit.lolerations[0].effect=NoSchedule \
 --set fluent-bit.tolerations[0].operator=Exists

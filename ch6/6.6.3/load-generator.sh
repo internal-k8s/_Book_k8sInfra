e0020232cd574d500a05f4b181bd010a408f3936
Seongju Mun <seongju.moon@linux.com>
2025-04-16 20:18:46 +0900

    add missing $

--- ch6/6.6.3/load-generator.sh
+++ ch6/6.6.3/load-generator.sh
@@ -16,6 +16,6 @@ echo "사용자 ID 1000의 점수를 읽어오는 API를 호출합니다."
 READ_RESPONSE=$(curl -X 'GET' -w '\n' \
   'http://192.168.1.13/api/v1/score/1000' \
   -H 'accept: application/json')
-echo "HTTP 응답: READ_RESPONSE"
+echo "HTTP 응답: $READ_RESPONSE"
 SCORE=$(echo $READ_RESPONSE | jq -r '.score')
 echo -e "사용자 ID 1000의 점수를 서버에서 읽어왔습니다.\n> 현재 점수: $SCORE"

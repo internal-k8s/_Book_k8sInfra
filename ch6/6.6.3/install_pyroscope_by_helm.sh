164f55746ce87d2fcb5433d5b025ada41ff071e6
Seongju Mun <seongju.moon@linux.com>
2025-05-02 21:15:50 +0900

    to fix subchart drilling value option.

--- ch6/6.6.3/install_pyroscope_by_helm.sh
+++ ch6/6.6.3/install_pyroscope_by_helm.sh
@@ -3,5 +3,5 @@ helm upgrade --reuse-values grafana-stack edu/grafana-stack \
 --namespace monitoring \
 --create-namespace \
 --set pyroscope.enabled=true \
---set pyroscope.persistence.enabled=true \
---set pyroscope.persistence.storageClassName="managed-nfs-storage"
+--set pyroscope.pyroscope.persistence.enabled=true \
+--set pyroscope.pyroscope.persistence.storageClassName="managed-nfs-storage"

a02398439bfb19fcf8ad78f66c96a8197136043f
Seongju Mun <seongju.moon@linux.com>
2025-05-02 20:38:56 +0900

    add persistence option.

--- ch6/6.6.3/install_pyroscope_by_helm.sh
+++ ch6/6.6.3/install_pyroscope_by_helm.sh
@@ -2,4 +2,6 @@
 helm upgrade --reuse-values grafana-stack edu/grafana-stack \
 --namespace monitoring \
 --create-namespace \
---set pyroscope.enabled=true
+--set pyroscope.enabled=true \
+--set pyroscope.persistence.enabled=true \
+--set pyroscope.persistence.storageClassName="managed-nfs-storage"

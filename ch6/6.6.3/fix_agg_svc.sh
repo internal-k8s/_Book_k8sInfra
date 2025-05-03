9e0b47a5006b72a04bea1064d5772938d2e9cb85
Seongju Mun <seongju.moon@linux.com>
2025-05-03 20:56:39 +0900

    fix image and deployment name.

--- ch6/6.6.3/fix_agg_svc.sh
+++ ch6/6.6.3/fix_agg_svc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Clone from github, agg-svc codebase"
+echo "Clone from github, colosseum-agg codebase"
 git clone https://github.com/k8s-edu/Bkv2_sub_colosseum
 cd Bkv2_sub_colosseum/Bkv2_sub_colosseum-agg
 
@@ -13,9 +13,9 @@ echo "final check before container build."
 git diff
 
 echo "build fix container image."
-docker build -f ./Dockerfile.profile -t 192.168.1.10:8443/library/aggregate-service:fix . --push
+docker build -f ./Dockerfile.profile -t 192.168.1.10:8443/library/colosseum-agg:fix . --push
 
 echo "re-deploy fix application."
-kubectl set image deploy agg-svc -n colosseum agg-svc=192.168.1.10:8443/library/aggregate-service:fix
+kubectl set image deploy colosseum-agg -n colosseum colosseum-agg=192.168.1.10:8443/library/colosseum-agg:fix
 
 cd -

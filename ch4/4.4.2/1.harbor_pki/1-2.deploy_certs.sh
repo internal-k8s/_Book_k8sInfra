5b6b4abf531be4da8d139f3848b415c42f455ff2
GNU Shim <geunwoo.j.shim@gmail.com>
2025-05-21 05:55:04 +0900

    fix harbor script

--- ch4/4.4.2/1.harbor_pki/1-2.deploy_certs.sh
+++ ch4/4.4.2/1.harbor_pki/1-2.deploy_certs.sh
@@ -8,13 +8,10 @@ HOST_CERT_DIR=/usr/local/share/ca-certificates
 # if 4.4.1 skipped, it should run. so add here. 
 apt-get install sshpass -y > /dev/null 2>&1
 
-echo "[Step 1/5] Create Harbor data directory"
-mkdir -p /harbor-data
-
-echo "[Step 2/5] Create Docker certificate directory"
+echo "[Step 1/4] Create Docker certificate directory"
 mkdir -p $DOCKER_CERT_DIR
 
-echo "[Step 3/5] Copying certificate to each of worker nodes"
+echo "[Step 2/4] Copying certificate to each of worker nodes"
 for i in {1..3}
   do
     echo "Copy to node w$i-k8s"
@@ -26,10 +23,10 @@ for i in {1..3}
 	    systemctl restart containerd
   done
 
-echo "[Step 4/5] Copy certificate to control plane"
+echo "[Step 3/4] Copy certificate to control plane"
 cp -f $HARBOR_FILE_DIR/ca.crt $DOCKER_CERT_DIR/ca.crt
 cp -f $HARBOR_FILE_DIR/ca.crt $HOST_CERT_DIR/harbor_ca.crt
 
-echo "[Step 5/5] Update certificate and restart containerd"
+echo "[Step 4/4] Update certificate list and restart containerd"
 update-ca-certificates 
 systemctl restart containerd

a4827cb722da78c245e788e82b19ae91cacfab35
root <root@cp-k8s>
2025-05-21 13:26:01 +0900

    fix docker related component version

--- ch4/4.2.1/install_docker.sh
+++ ch4/4.2.1/install_docker.sh
@@ -2,8 +2,13 @@
 
 # Docker version 
 docker_V='5:24.0.6-1~ubuntu.22.04~jammy' 
+buildx_V='0.23.0-1~ubuntu.22.04~jammy'
+compose_V='2.35.1-1~ubuntu.22.04~jammy'
 
 # install & enable docker 
 apt-get update 
-apt-get install docker-ce=$docker_V docker-ce-cli=$docker_V -y 
-
+apt-get install docker-ce=$docker_V \
+                docker-ce-cli=$docker_V \
+                docker-ce-rootless-extras=$docker_V \
+                docker-buildx-plugin=$buildx_V \
+                docker-compose-plugin=$compose_V  -y

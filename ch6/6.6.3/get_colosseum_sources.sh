1f65c5ebc5b4cf73dc7f5eefb78c91e97e1fa497
Seongju Mun <seongju.moon@linux.com>
2025-05-24 16:10:24 +0900

    fix message format.

--- ch6/6.6.3/get_colosseum_sources.sh
+++ ch6/6.6.3/get_colosseum_sources.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 git clone https://github.com/k8s-edu/Bkv2_sub_colosseum
 cd Bkv2_sub_colosseum
-ls -1
 find /root/_Book_k8sInfra -regex ".*\.\(sh\)" -exec chmod 700 {} \;
-echo "Now go to ~/_Book_k8sInfra/ch6/6.6.3/Bkv2_sub_colosseum/Bkv2_sub_colosseum-agg"
+echo "ⓘ Colosseum sources at: ~/_Book_k8sInfra/ch6/6.6.3/Bkv2_sub_colosseum"

2731bdd3031b985915f5c1418e301a1cd071f970
Seongju Mun <seongju.moon@linux.com>
2025-06-01 11:24:42 +0900

    fix remove command.

--- ch6/6.6.3/uninstall_colosseum_apps.sh
+++ ch6/6.6.3/uninstall_colosseum_apps.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 echo "[Step 1/4] Uninstalling Load-generator..."
-helm uninstall  load-generator edu/load-generator \
+helm uninstall  load-generator \
 --namespace colosseum > /dev/null 2>&1
 
 echo "[Step 2/4] Uninstalling Colosseum apps..."
-helm uninstall colosseum edu/colosseum \
+helm uninstall colosseum \
 --namespace colosseum 
 
 echo "[Step 3/4] Uninstalling Redis..."
-helm uninstall redis edu/redis \
+helm uninstall redis \
 --namespace colosseum > /dev/null 2>&1
 
 echo "[Step 4/4] Remove colosseum namespace."

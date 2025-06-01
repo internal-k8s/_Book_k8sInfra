ec57d11210f63d901e3b5e96bd42c99833e5ff67
Seongju Mun <seongju.moon@linux.com>
2025-06-01 11:18:26 +0900

    Remove logic fix and add message.

--- ch6/6.6.3/uninstall_colosseum_apps.sh
+++ ch6/6.6.3/uninstall_colosseum_apps.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
-echo "[Step 1/3] Uninstalling Load-generator..."
+echo "[Step 1/4] Uninstalling Load-generator..."
 helm uninstall  load-generator edu/load-generator \
---namespace colosseum \
---create-namespace > /dev/null 2>&1
+--namespace colosseum > /dev/null 2>&1
 
-echo "[Step 2/3] Uninstalling Colosseum apps..."
+echo "[Step 2/4] Uninstalling Colosseum apps..."
 helm uninstall colosseum edu/colosseum \
---namespace colosseum \
---create-namespace \
---set monitoring.mode=trace
+--namespace colosseum 
 
-echo "[Step 3/3] Uninstalling Redis..."
+echo "[Step 3/4] Uninstalling Redis..."
 helm uninstall redis edu/redis \
---namespace colosseum \
---create-namespace > /dev/null 2>&1
+--namespace colosseum > /dev/null 2>&1
 
-echo "Remove colosseum namespace."
+echo "[Step 4/4] Remove colosseum namespace."
 kubectl delete namespace colosseum

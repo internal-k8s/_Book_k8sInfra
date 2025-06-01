dc33f5362532c33bb5d331c897a060963fb8364e
Seongju Mun <seongju.moon@linux.com>
2025-06-01 11:06:02 +0900

    fix deploy order and add uninstall script.

--- ch6/6.6.1/install_log_app_by_helm.sh
+++ ch6/6.6.1/install_log_app_by_helm.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-echo "Deploy redis."
+echo "[Step 1/3] Deploying Redis..."
 helm install redis edu/redis \
 --namespace colosseum \
 --create-namespace > /dev/null 2>&1
 
-echo "Wait for redis is ready."
+echo "Wait for Redis is ready."
 kubectl -n colosseum wait deployment/redis --for=condition=available
 
-echo "Deploy colosseum apps."
+echo "[Step 2/3] Deploying Colosseum apps..."
 helm install colosseum edu/colosseum \
 --namespace colosseum \
 --set monitoring.mode=log
+
+echo "[Step 3/3] Deploying Load-generator..."
+helm install load-generator edu/load-generator \
+--namespace colosseum \
+--create-namespace > /dev/null 2>&1

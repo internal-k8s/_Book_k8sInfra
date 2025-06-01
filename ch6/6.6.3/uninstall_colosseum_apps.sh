6ea502a9cc4ed981e57631d18222cbd56874cbef
Seongju Mun <seongju.moon@linux.com>
2025-06-01 11:11:59 +0900

    fix uninstaller logic.

--- ch6/6.6.3/uninstall_colosseum_apps.sh
+++ ch6/6.6.3/uninstall_colosseum_apps.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 echo "[Step 1/3] Uninstalling Load-generator..."
-helm upgrade --install  load-generator edu/load-generator \
+helm uninstall  load-generator edu/load-generator \
 --namespace colosseum \
 --create-namespace > /dev/null 2>&1
 
 echo "[Step 2/3] Uninstalling Colosseum apps..."
-helm upgrade --install colosseum edu/colosseum \
+helm uninstall colosseum edu/colosseum \
 --namespace colosseum \
 --create-namespace \
 --set monitoring.mode=trace
 
 echo "[Step 3/3] Uninstalling Redis..."
-helm install redis edu/redis \
+helm uninstall redis edu/redis \
 --namespace colosseum \
 --create-namespace > /dev/null 2>&1
 

90b761c6e5f33ff9fce649bd2b9a4176179f0764
Seongju Mun <seongju.moon@linux.com>
2025-06-01 11:06:02 +0900

    fix deploy order and add uninstall script.

--- ch6/6.6.2/install_trace_app_by_helm.sh
+++ ch6/6.6.2/install_trace_app_by_helm.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
-echo "Deploy redis."
+echo "[Step 1/3] Deploying Redis..."
 helm upgrade --install redis edu/redis \
 --namespace colosseum \
 --create-namespace > /dev/null 2>&1
 
-echo "Wait for redis is ready."
+echo "Wait for Redis is ready."
 kubectl -n colosseum wait deployment/redis --for=condition=available
 
-echo "Deploy colosseum apps."
+echo "[Step 2/3] Deploying Colosseum apps..."
 helm upgrade --install colosseum edu/colosseum \
 --namespace colosseum \
 --create-namespace \
 --set monitoring.mode=trace
+
+echo "[Step 3/3] Deploying Load-generator..."
+helm upgrade --install  load-generator edu/load-generator \
+--namespace colosseum \
+--create-namespace > /dev/null 2>&1
---
+++ ch6/6.6.3/uninstall_grafana_stack.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+helm uninstall grafana-stack -n monitoring

036ba8ceac4f86ff2b7f1b7a8afd0d765ff1777c
Seongju Mun <seongju.moon@linux.com>
2025-06-01 13:08:03 +0900

    fix output message.

--- ch6/6.6.1/install_log_app_by_helm.sh
+++ ch6/6.6.1/install_log_app_by_helm.sh
@@ -7,7 +7,7 @@ helm install redis edu/redis \
 kubectl -n colosseum wait deployment/redis --for=condition=available > /dev/null 2>&1
 echo "Done"
 
-echo "[Step 2/3] Deploying Colosseum apps..."
+echo "[Step 2/3] Deploying Colosseum app..."
 helm install colosseum edu/colosseum \
 --namespace colosseum \
 --set monitoring.mode=log

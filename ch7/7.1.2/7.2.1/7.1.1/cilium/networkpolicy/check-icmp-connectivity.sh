76d5e4ce3f832b93d50034035578d035a53736df
Hoon Jo <pagaia@hotmail.com>
2025-06-08 12:51:02 +0900

    archived lens to old dir

---
+++ ch7/7.1.2/7.2.1/7.1.1/cilium/networkpolicy/check-icmp-connectivity.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ALLOW_IP=$(kubectl get pods net-conn-allow -o jsonpath="{.status.podIP}")
+DENY_IP=$(kubectl get po net-conn-deny -o jsonpath="{.status.podIP}")
+
+echo "현재 배포된 파드 상태"
+echo "=============================================="
+kubectl get po -o wide --show-labels
+echo "=============================================="
+
+echo "> [console to allow] ping 3회 테스트"
+kubectl exec net-conn-console -- ping -c 3 $ALLOW_IP
+
+echo -e "\n\n"
+
+echo "> [console to deny] ping 3회 테스트"
+kubectl exec net-conn-console -- ping -c 3 $DENY_IP

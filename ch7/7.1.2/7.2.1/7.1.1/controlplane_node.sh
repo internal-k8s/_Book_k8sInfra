8749cd31294d73c0a215b971c619e3c95ec6ef78
Hoon Jo <pagaia@hotmail.com>
2025-06-08 12:51:02 +0900

    archived lens to old dir

---
+++ ch7/7.1.2/7.2.1/7.1.1/controlplane_node.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# init kubernetes 
+kubeadm init --token 123456.1234567890123456 --token-ttl 0 \
+             --skip-phases=addon/kube-proxy \
+             --pod-network-cidr=172.16.0.0/16 \
+             --apiserver-advertise-address=192.168.1.10 
+
+# config for control-plane node only 
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+  
+# kubectl completion on bash-completion dir
+kubectl completion bash > /etc/bash_completion.d/kubectl
+
+# alias kubectl to k 
+echo 'alias k=kubectl'               >> ~/.bashrc
+echo "alias ka='kubectl apply -f'"   >> ~/.bashrc
+echo "alias kg-po-ip-stat-no='kubectl get pods -o=custom-columns=\
+NAME:.metadata.name,IP:.status.podIP,STATUS:.status.phase,NODE:.spec.nodeName'" \
+                                     >> ~/.bashrc 
+echo 'complete -F __start_kubectl k' >> ~/.bashrc
+
+# git clone book source 
+git clone https://github.com/internal-k8s/_Book_k8sInfra.git 
+mv /home/vagrant/_Book_k8sInfra $HOME
+find $HOME/_Book_k8sInfra -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+
+# make rerepo-Book-k8sInfra and input proper permission
+cat <<EOF > /usr/local/bin/rerepo-Book_k8sInfra
+#!/usr/bin/env bash
+rm -rf $HOME/_Book_k8sInfra
+git clone https://github.com/internal-k8s/_Book_k8sInfra.git $HOME/_Book_k8sInfra
+find $HOME/_Book_k8sInfra -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+EOF
+chmod 700 /usr/local/bin/rerepo-Book_k8sInfra
+
+
+# CNI raw address & config for kubernetes's network 
+kubectl apply -f /root/_Book_k8sInfra/ch7/7.1.1/cilium/cilium-v1.16.2-w-hubble.yaml

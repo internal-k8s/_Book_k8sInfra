98177990fb16210b91a245dc8c352497a3ffb8f1
Hoon Jo <pagaia@hotmail.com>
2025-06-16 16:47:18 +0900

    upt installation for headlamp

---
+++ ch7/7.1.3/.cmd
@@ -0,0 +1,7 @@
+# headlamp
+## installation 
+### MacOS
+### https://formulae.brew.sh/cask/headlamp
+### brew install --cask headlamp 
+### https://github.com/Homebrew/homebrew-cask/blob/master/Casks/h/headlamp.rb
+brew install --cask ./headlamp-v0.31.1/headlamp.rb

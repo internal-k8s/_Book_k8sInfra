2aafad89c67ce4d91fa14a9b6a815c49f3b33421
Hoon Jo <pagaia@hotmail.com>
2025-06-20 10:28:06 +0900

    upt .cmd

---
+++ ch2/2.1.1/arm64/.cmd
@@ -0,0 +1,8 @@
+# virtualbox 
+## installation 
+### MacOS 
+### https://formulae.brew.sh/cask/virtualbox 
+### brew install --cask virtualbox 
+### https://github.com/Homebrew/homebrew-cask/blob/master/Casks/v/virtualbox.rb
+### virtualbox v7.1.10
+brew install --cask ./virtualbox-v7.1.10/virtualbox.rb

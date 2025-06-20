080a4fa91e6fd372c0b7ded487364e6ad01b25f0
Hoon Jo <pagaia@hotmail.com>
2025-06-20 09:48:55 +0900

    upt .cmd

--- ch7/7.1.3/.cmd
+++ ch7/7.1.3/.cmd
@@ -1,5 +1,10 @@
 # headlamp
 ## installation 
+### Windows
+### https://winstall.app/apps/Headlamp.Headlamp
+### winget install --id=Headlamp.Headlamp  -e
+winget install headlamp -v 0.31.1
+
 ### MacOS
 ### https://formulae.brew.sh/cask/headlamp
 ### brew install --cask headlamp 

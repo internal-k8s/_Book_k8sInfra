566ba447f674ef25eaacaa2ea824574b66bacc4a
Seongju Mun <seongju.moon@linux.com>
2025-06-21 16:22:44 +0900

    Change directory structure for operator.

--- ch7/7.2.3/install-prometheus_operator_by_helm.sh
+++ ch7/7.2.3/install-prometheus_operator_by_helm.sh
@@ -3,4 +3,5 @@
 helm install prometheus-stack edu/kube-prometheus-stack  \
 --namespace=monitoring \
 --create-namespace \
--f $HOME/_Book_k8sInfra/ch7/7.1.2/prometheus-operator-values.yaml
+-f $HOME/_Book_k8sInfra/ch7/7.2.3/prometheus-operator-values.yaml
+

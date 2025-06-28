d1da50b684a72f4d2a60c8c3dd23558d7d9ae621
Seongju Mun <seongju.moon@linux.com>
2025-06-28 12:21:56 +0900

    reorgnize cilium lab.

---
+++ ch7/7.2.2/cleanup_7.2.2_tasks.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+HUBBLE_SVC="$(kubectl -n kube-system get svc hubble-ui)"
+
+echo "First, check hubble-ui service."
+
+if [ "$HUBBLE_SVC" != "" ]; then
+  echo "Found hubble-ui service."
+  echo "Check service type need to change action."
+  SERVICE_TYPE="$(kubectl -n kube-system get svc hubble-ui -o jsonpath='{.spec.type}' | tr '[:upper:]' '[:lower:]')"
+  if [ "$SERVICE_TYPE" == "loadbalancer" ]; then
+    kubectl patch  -n kube-system svc/hubble-ui -p '{"spec":{"type":"ClusterIP"}}'
+    echo "Hubble ui change type $SERVICE_TYPE to ClusterIP."
+  else
+     echo "Hubble ui already set ClusterIP."
+  fi
+  echo "Please take a look below details connection information."
+  kubectl -n kube-system get svc hubble-ui
+else
+  echo "Cannot found hubble-ui service."
+  echo "Please check is active cilium cni on this kubernetes cluster."
+  echo "You may check 'kubectl get pods -n kube-system | grep cilium' command."
+fi
+
+echo "Second, delete the pods, for cleanup and move forward to next task."
+kubectl delete -f net-conn-pods.yaml
+echo "Delete the pod."
+
+echo "Check the pod is deleted."
+kubectl get pods -o wide
+
+echo "Let's move forward to next task."

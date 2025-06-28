3c4f8c2f822a8f53e729fbae0639656d737229b6
Seongju Mun <seongju.moon@linux.com>
2025-06-28 12:21:56 +0900

    reorgnize cilium lab.

--- ch7/7.2.2/configure_cilium_connectvity_env.sh.sh
+++ ch7/7.2.2/configure_cilium_connectvity_env.sh.sh
@@ -2,6 +2,8 @@
 
 HUBBLE_SVC="$(kubectl -n kube-system get svc hubble-ui)"
 
+echo "First, check hubble-ui service."
+
 if [ "$HUBBLE_SVC" != "" ]; then
   echo "Found hubble-ui service."
   echo "Check service type need to change action."
@@ -19,3 +21,17 @@ else
   echo "Please check is active cilium cni on this kubernetes cluster."
   echo "You may check 'kubectl get pods -n kube-system | grep cilium' command."
 fi
+
+echo "Second, Prepare the pods, for check cilium network connectivity and network policy."
+kubectl apply -f net-conn-pods.yaml
+echo "Create the pod."
+
+echo "Check the pod is running."
+kubectl get pods -o wide
+
+echo "Yon can go to next step, joureny to cilium and hubble ui."
+echo "The hubble ui is available at http://$(kubectl -n kube-system get svc hubble-ui -o jsonpath='{.status.loadBalancer.ingress[0].ip})"
+
+echo "You can check the network connectivity and network policy."
+echo "Let's check the network connectivity."
+

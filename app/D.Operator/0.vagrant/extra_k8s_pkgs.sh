4e88ea81506aac4eb03d128c97782ec4aff9d6eb
Hoon Jo <pagaia@hotmail.com>
2026-04-04 16:11:10 +0900

    migrate NFS provisioner: nfs-subdir-external-provisioner v4.0.0 → csi-driver-nfs v4.12.1
    - Add csi-driver-nfs-v4.12.1.yaml (CSIDriver + RBAC + controller + node DaemonSet)
    - Update storageclass.yaml: provisioner nfs.csi.k8s.io, server/share params, nfsvers=4.1
    - Update ch3/4/5/6/.init_infra.sh: use new yaml + 30s CSI readiness wait
    - Update ch7/7.1.1/extra_k8s_pkgs.sh and app/D.Operator: same migration

--- app/D.Operator/0.vagrant/extra_k8s_pkgs.sh
+++ app/D.Operator/0.vagrant/extra_k8s_pkgs.sh
@@ -2,7 +2,8 @@
 
 # Setup nfs-provisioner for elasticsearch lab
 sh -c "$HOME/_Book_k8sInfra/ch3/3.4.3/nfs_exporter.sh dynamic-vol"
-kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/nfs-subdir-external-provisioner-v4.0.0.yaml
+kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/csi-driver-nfs-v4.12.1.yaml
+echo "Waiting 30secs for CSI driver NFS" ; sleep 30
 kubectl create -f $HOME/_Book_k8sInfra/ch3/3.4.3/storageclass.yaml
 kubectl annotate storageclass managed-nfs-storage storageclass.kubernetes.io/is-default-class=true
 # Setup loadbalancer

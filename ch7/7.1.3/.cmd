723adbca3ee6b0cd97147ad5f1067b5ca289ed7a
Hoon Jo <pagaia@hotmail.com>
2026-04-20 20:29:23 +0900

    headlamp .cmd: add HOMEBREW_DEVELOPER=1 workaround for local cask install

--- ch7/7.1.3/.cmd
+++ ch7/7.1.3/.cmd
@@ -6,7 +6,7 @@
 winget install headlamp -v 0.41.0
 
 ### MacOS
-### DMG 직접 설치 (Homebrew cask는 Gatekeeper 미통과로 deprecated)
-### https://github.com/kubernetes-sigs/headlamp/releases/tag/v0.41.0
-### arm64: Headlamp-0.41.0-mac-arm64.dmg
-### x64:   Headlamp-0.41.0-mac-x64.dmg
+### Homebrew cask 저장소 정책 변경으로 로컬 .rb 파일 설치 시 HOMEBREW_DEVELOPER=1 필요
+### https://formulae.brew.sh/cask/headlamp
+export HOMEBREW_DEVELOPER=1
+brew install --cask ./headlamp-v0.41.0/headlamp.rb

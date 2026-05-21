12f82c7761eeb29382b8b3472f4fbda5331ab5ed
Hoon Jo <pagaia@hotmail.com>
2026-05-21 19:39:46 +0900

    ch2: bump VirtualBox v7.2.8, Vagrant v2.4.9, Tabby v1.0.234
    - Add virtualbox-v7.2.8/virtualbox.rb (arm64 + intel SHA256)
    - Add vagrant-v2.4.9/vagrant.rb (arm64 + intel SHA256)
    - Add tabby-v1.0.234/tabby.rb (arm64 + intel SHA256)
    - Update arm64/.cmd to reference virtualbox-v7.2.8

--- ch2/2.1.1/arm64/.cmd
+++ ch2/2.1.1/arm64/.cmd
@@ -4,5 +4,5 @@
 ### https://formulae.brew.sh/cask/virtualbox 
 ### brew install --cask virtualbox 
 ### https://github.com/Homebrew/homebrew-cask/blob/master/Casks/v/virtualbox.rb
-### virtualbox v7.1.10
-brew install --cask ./virtualbox-v7.1.10/virtualbox.rb
+### virtualbox v7.2.8
+brew install --cask ./virtualbox-v7.2.8/virtualbox.rb

e2ae0df8799b37fa67042741f18769acf5dc43a0
Seongju Mun <seongju.moon@linux.com>
2026-05-24 16:32:38 +0900

    support multi-archtecture for argo-rollouts lab.

--- ch7/7.4.2/install_argo_rollouts.sh
+++ ch7/7.4.2/install_argo_rollouts.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [[ "$(uname -m)" =~ ^(arm64|aarch64)$ ]]; then
+  ARCH="arm64"
+else
+  ARCH="amd64"
+fi
+
 echo "🚀 Install Argo Rollouts controller."
 kubectl create namespace argo-rollouts 2>/dev/null
 kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/download/v1.7.2/install.yaml
@@ -12,7 +18,7 @@ echo "✅ Argo Rollouts controller installed."
 echo ""
 echo "🔧 Install kubectl argo rollouts plugin."
 curl -sLo /usr/local/bin/kubectl-argo-rollouts \
-  https://github.com/argoproj/argo-rollouts/releases/download/v1.7.2/kubectl-argo-rollouts-linux-amd64
+  https://github.com/argoproj/argo-rollouts/releases/download/v1.7.2/kubectl-argo-rollouts-linux-${ARCH}
 chmod +x /usr/local/bin/kubectl-argo-rollouts
 
 PLUGIN_VER="$(kubectl argo rollouts version --short 2>/dev/null)"

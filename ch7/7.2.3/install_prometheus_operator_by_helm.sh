9442345991e1e9f6c8d9667d1e3f02032fa79102
Seongju Mun <seongju.moon@linux.com>
2026-06-07 14:15:06 +0900

    remove ip address announce.

--- ch7/7.2.3/install_prometheus_operator_by_helm.sh
+++ ch7/7.2.3/install_prometheus_operator_by_helm.sh
@@ -6,5 +6,3 @@ helm install prometheus-stack edu/kube-prometheus-stack \
   --create-namespace \
   -f $HOME/_Book_k8sInfra/ch7/7.2.3/prometheus-operator-values.yaml
 
-echo ""
-echo "Prometheus available at http://$(kubectl get svc -n monitoring -l app=kube-prometheus-stack-prometheus -o jsonpath='{.items[0].status.loadBalancer.ingress[0].ip}')"

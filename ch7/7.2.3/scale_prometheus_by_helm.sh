ac9f64af0f9a2a45f84a5a9108a8ce105dfa0922
Seongju Mun <seongju.moon@linux.com>
2026-06-07 10:40:33 +0900

    more clearfiy

--- ch7/7.2.3/scale_prometheus_by_helm.sh
+++ ch7/7.2.3/scale_prometheus_by_helm.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Configure Prometheus HA with replicas=2."
+DESIRED=2
+
+CURRENT=$(kubectl get prometheus -n monitoring -o jsonpath='{.items[0].spec.replicas}')
+CURRENT=${CURRENT:-1}
+
+echo "Scale Prometheus replicas from ${CURRENT} to ${DESIRED}."
 helm upgrade prometheus-stack edu/kube-prometheus-stack \
   --namespace=monitoring \
   -f $HOME/_Book_k8sInfra/ch7/7.2.3/prometheus-operator-values.yaml \
-  --set prometheus.prometheusSpec.replicas=2
+  --set prometheus.prometheusSpec.replicas=${DESIRED}

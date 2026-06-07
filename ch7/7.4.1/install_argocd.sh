c7688c60bbbdeb8b3b2d78d6be8e73f60a1fb650
Seongju Mun <seongju.moon@linux.com>
2026-06-07 14:14:30 +0900

    renovation argocd.

--- ch7/7.4.1/install_argocd.sh
+++ ch7/7.4.1/install_argocd.sh
@@ -8,9 +8,6 @@ kubectl apply --server-side --force-conflicts -n argocd \
 echo "Wait for ArgoCD to be ready..."
 kubectl rollout status deployment/argocd-server -n argocd --timeout=180s
 
-echo "Expose ArgoCD server as LoadBalancer."
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-
 PASSWORD="$(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)"
 ENDPOINT="$(kubectl get service argocd-server -n argocd -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
 

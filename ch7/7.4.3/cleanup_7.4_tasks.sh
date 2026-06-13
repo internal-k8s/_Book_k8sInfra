8333ab82489e5fe460727fa224b33b713e60e468
Seongju Mun <seongju.moon@linux.com>
2026-06-14 07:54:55 +0900

    echo message update.

--- ch7/7.4.3/cleanup_7.4_tasks.sh
+++ ch7/7.4.3/cleanup_7.4_tasks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "🧹 [7.4] ArgoCD / Argo Rollouts 실습 리소스 정리"
+echo "🧹 [7.4] 아르고 CD / 아르고 롤아웃 실습 리소스 정리"
 
 # 7.4.3 카나리 Rollout 삭제
 kubectl delete -f $HOME/_Book_k8sInfra/ch7/7.4.3/ro-canary.yaml 2>/dev/null

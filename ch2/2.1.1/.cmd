b245a234a53811745ea0d4327ee9fc615824cb0c
Hoon Jo <pagaia@hotmail.com>
2026-07-04 09:54:13 +0900

    ch2/2.1.2: move vagrant install files from 2.1.1
    Vagrant install files (vagrant-v2.4.7, vagrant-v2.4.9, and the
    vagrant half of .cmd) were nested under ch2/2.1.1 (버추얼박스
    설치하기), but they belong under ch2/2.1.2 (베이그런트 설치하기)
    per the book's section layout and ch2/README.md.

--- ch2/2.1.1/.cmd
+++ ch2/2.1.1/.cmd
@@ -10,17 +10,3 @@ winget install Oracle.VirtualBox -v 7.2.8
 ### virtualbox v7.2.8
 export HOMEBREW_DEVELOPER=1
 brew install --cask ./virtualbox-v7.2.8/virtualbox.rb
-
----
-
-# vagrant
-## installation
-### Windows
-### https://winstall.app/apps/Hashicorp.Vagrant
-winget install Hashicorp.Vagrant -v 2.4.9
-
-### MacOS
-### https://formulae.brew.sh/cask/vagrant
-### vagrant v2.4.9
-export HOMEBREW_DEVELOPER=1
-brew install --cask ./vagrant-v2.4.9/vagrant.rb
---
+++ ch2/2.1.2/.cmd
@@ -0,0 +1,11 @@
+# vagrant
+## installation
+### Windows
+### https://winstall.app/apps/Hashicorp.Vagrant
+winget install Hashicorp.Vagrant -v 2.4.9
+
+### MacOS
+### https://formulae.brew.sh/cask/vagrant
+### vagrant v2.4.9
+export HOMEBREW_DEVELOPER=1
+brew install --cask ./vagrant-v2.4.9/vagrant.rb
